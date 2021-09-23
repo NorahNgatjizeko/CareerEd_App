c39d46bad722473756cacbde45347b2538312639
norah ngatjizeko <tjeripo86@live.com>
2021-09-24 00:47:20 +0900

    guest update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-user = User.new(
-  name: "Admin user",
-  email: "admin@mail.com",
-  password: "password",
-  password_confirmation: "password",
-  admin: true
-)
-user.save!
